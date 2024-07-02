40390a1f62ffd86b1bad35f1114580564cc8b854
Mayank K <mayank@mc-awesome.com>
2024-07-02 17:49:26 +0400

    Debugging Nav

--- medusajs-storefront/src/modules/layout/templates/index.tsx
+++ medusajs-storefront/src/modules/layout/templates/index.tsx
@@ -8,7 +8,7 @@ const Layout: React.FC<{
 }> = ({ children }) => {
   return (
     <div>
-      <Nav />
+      {/* <Nav /> */}
       <main className="relative">{children}</main>
       <Footer />
     </div>
